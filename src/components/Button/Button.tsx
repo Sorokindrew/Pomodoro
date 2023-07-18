d4e0631f52fd78056d8341c0c52b403985525a6c
Andrey Sorokin <sorokindrew@mail.ru>
2023-07-18 19:59:52 +0300

    Redux added

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { FormEvent } from 'react'
 import styles from './button.module.css'
 
 interface IButtonProps {
     text: string
+    onClick: (e: FormEvent) => void
 }
 
 
 
-export function Button({text}: IButtonProps) {
+export function Button({text, onClick}: IButtonProps) {
     return (
-        <button className={styles.button}>
+        <button className={styles.button} onClick={onClick}>
             {text}
         </button>
     )
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { ChangeEvent, FormEvent } from 'react'
 import styles from './home.module.css'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store/reducer'
+import { Button } from '../Button/Button'
+import { useDispatch } from 'react-redux'
+import { addTask, updateInputText } from '../../store/actions'
 
 
 
 export function Home() {
+const inputText = useSelector<RootState, string>(state => state.inputText)
+const dispatch = useDispatch()
+const tasks = useSelector<RootState,string[]>(state=>state.taskList)
+
+const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch(updateInputText(e.target.value))
+}
+
+const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    dispatch(addTask(inputText))
+    dispatch(updateInputText(''))
+}
+
     return (
         <div className={styles.home}>
            Home screen
+           <form>
+           <input type="text" onChange={handleChange} value={inputText}/>
+           <Button text={'Добавить задачу'} onClick={handleSubmit}/>
+           </form>
+           <ul>
+            {tasks.map(el=> {
+                return (
+                    <li>{el}</li>
+                )
+            })}
+           </ul>
+           
+          
+           
         </div>
     )
 }
